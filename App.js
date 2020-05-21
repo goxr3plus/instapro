bfa64a2a6a8973f31288d66d9a9b48f31a36ec3b
Kentros Alexandros <Alexandros.KENTROS@intrasoft-intl.com>
2020-05-21 16:36:12 +0300

    First Commit

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [name, setName] = useState("Maria");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{name}</Text>
+      <Button title="Change text" onPress={(e) => setName("Pitska Kari2")} ></Button>
     </View>
   );
 }
@@ -12,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
